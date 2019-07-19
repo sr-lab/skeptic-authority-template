c9a9ee6a0198cbdd7dacb61cf36b5bdfcec56612
Saul Johnson <saul.a.johnson@gmail.com>
2019-07-19 22:35:55 +0100

    Tidy up Authority.v

--- src/Authority.v
+++ src/Authority.v
@@ -18,25 +18,25 @@ Definition Configuration : Type :=
 Definition transform (config : Configuration) : list MetaRule :=
   [].
 
-Fixpoint complies_all (mr : list MetaRule) (p : string) : bool :=
-  match mr with
-  | x :: mr' => x p && complies_all mr' p
+Fixpoint complies_all (rules : list MetaRule) (pwd : string) : bool :=
+  match rules with
+  | rule :: rules' => rule pwd && complies_all rules' pwd
   | _ => true
   end.
 
-Fixpoint determine (p : list MetaRule) (n : nat) : C.t System.effect unit :=
+Fixpoint determine (rules : list MetaRule) (n : nat) : C.t System.effect unit :=
   match n with
   | S n' =>
-    let! pwd := System.read_line in
-    match pwd with
+    let! pwd_in := System.read_line in
+    match pwd_in with
     | None => ret tt
-    | Some pwd' =>
-    do! System.log (if complies_all p (LString.to_string pwd') then (LString.s "permitted") else (LString.s "Prohibited")) in
-          determine p n'
+    | Some pwd_in' =>
+      let pwd := LString.to_string pwd_in' in
+      let msg := if complies_all rules pwd then "true" else "false" in
+      do! System.log (LString.s msg) in
+      determine rules n'
     end
-  
-  | Z =>
-    System.log (LString.s "End of input.")
+  | Z => ret tt
   end.
 
 Definition lookup_config (name : string) : option Configuration :=
@@ -47,10 +47,10 @@ Definition lookup_config (name : string) : option Configuration :=
 
 Definition authority (argv : list LString.t) : C.t System.effect unit :=
   match argv with
-  | _ :: policy :: count :: [] =>
-    match lookup_config (LString.to_string policy) with
-      | Some dd =>
-        determine (transform dd) (nat_of_string (LString.to_string count))
+  | _ :: config_name :: count :: [] =>
+    match lookup_config (LString.to_string config_name) with
+      | Some config =>
+        determine (transform config) (nat_of_string (LString.to_string count))
       | None => ret tt
     end
   | _ => ret tt
